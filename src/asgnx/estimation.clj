0f84149449fa57a079867aa9b1da1580c392fde6
Bailey Pearson <bailey.pearson@vanderbilt.edu>
2018-10-21 13:15:16 -0500

    created skeleton for estimation.clj

--- src/asgnx/estimation.clj
+++ src/asgnx/estimation.clj
@@ -3,3 +3,31 @@
             [asgnx.kvstore :as kvstore]
 	)
 )
+
+;; This module is responsible for managing the wait times associated with campus
+;; dining locations.  Wait times for each dining location are stored as a list of
+;; maps in the following structure:
+;; 		{
+;; 			:locations { :pub [{ :date _____ :wait _____ } ] }
+;; 		}
+;;		
+;;		:date is a date/time object associated with the time of reporting
+;;		:wait is the actual wait time reported
+;; 		
+;; Each day, the wait times are cleared for each dining location.
+
+
+(defn add-wait [location wait]
+	)
+
+(defn get-wait-loc [location]
+	)
+
+(defn get-wait []
+	)
+
+(defn update-wait-loc [location]
+	)
+
+(defn update-wait []
+	)
